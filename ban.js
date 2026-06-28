0191ed5bc1cb957716befd944dc4f76765bb7d02
facebookruhin4-dev <facebookruhin4@gmail.com>
2026-06-28 15:33:07 +0600

    Implement banned words checker in ban.js
    Added a function to check for banned words in messages.

--- ban.js
+++ ban.js
@@ -1,1 +1,32 @@
+// ban.js
+// এখানে আপনার ইচ্ছামতো যত খুশি খারাপ শব্দ বা গালি যোগ করতে পারবেন ওস্তাদ
+const bannedWords = [
+    "xxx", 
+    "sex", 
+    "বাল", 
+    "চুদ", 
+    "বগা", 
+    "সেক্স", 
+    "এক্সক্সক্স"
+];
+
+/**
+ * মেসেজে কোনো নিষিদ্ধ শব্দ আছে কিনা তা চেক করার ফাংশন
+ * @param {string} text 
+ * @returns {boolean}
+ */
+function hasBannedWord(text) {
+    if (!text) return false;
+    
+    // মেসেজটিকে ছোট হাতের অক্ষরে কনভার্ট করে চেক করা (যাতে Sex বা SEX দিলেও ধরে ফেলে)
+    const lowerText = text.toLowerCase();
+    
+    // কোনো একটি নিষিদ্ধ শব্দ মেসেজের ভেতর থাকলেই true রিটার্ন করবে
+    return bannedWords.some(word => lowerText.includes(word));
+}
+
+module.exports = {
+    bannedWords,
+    hasBannedWord
+};
 
